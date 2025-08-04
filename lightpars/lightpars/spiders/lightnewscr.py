7dd47c6767f06857e3c8315e1c3c90ecbdd0d28e
Юлия (Джулия) <sixquestions666@gmail.com>
2025-08-04 22:52:42 +0300

    Change the site
    From hoff.ru to divan.ru

--- lightpars/lightpars/spiders/lightnewscr.py
+++ lightpars/lightpars/spiders/lightnewscr.py
@@ -3,8 +3,17 @@ import scrapy
 
 class LightnewscrSpider(scrapy.Spider):
     name = "lightnewscr"
-    allowed_domains = ["https://hoff.ru"]
-    start_urls = ["https://hoff.ru/catalog/tovary_dlya_doma/osveschenie/"]
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        lights = response.css("div.LlPhw")
+        for light in lights:
+            yield {
+                "name": light.css("div.WdR1o span::text").get(),
+                "price": light.css('div.ui-LD-ZU KIkOH span::text').get(),
+                "url": light.css('a').attrib['href']
+
+            }
+
+
